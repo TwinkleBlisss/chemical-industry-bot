850ced9385bdba173368a606f761b80c019a91a5
TwinkleBlisss <yarov.tim@yandex.ru>
2023-12-22 19:04:41 +0300

    Little changes

--- handlers/general_commands.py
+++ handlers/general_commands.py
@@ -1,6 +1,6 @@
 from aiogram import types, F, Router
 from aiogram.enums import ParseMode
-from aiogram.filters import Command
+from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from random import randint
 from keyboards.general_keyboards import (
@@ -17,19 +17,13 @@ router = Router()
 
 
 # /start
-@router.message(Command("start"))
+@router.message(StateFilter(None), Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer(text.start_phrase)
 
 
-# /help
-@router.message(Command("help"))
-async def cmd_help(message: types.Message):
-    await message.answer(text.commands_phrase)
-
-
 # /bot_info
-@router.message(Command("bot_info"))
+@router.message(StateFilter(None), Command("bot_info"))
 async def cmd_bot_info(message: types.Message):
     await message.answer(
         'Выберите ссылку',
@@ -38,7 +32,7 @@ async def cmd_bot_info(message: types.Message):
 
 
 # /id
-@router.message(Command("id"))
+@router.message(StateFilter(None), Command("id"))
 async def cmd_id(message: types.Message):
     await message.answer(
         f"Ваш id: {message.from_user.id}"
@@ -46,7 +40,7 @@ async def cmd_id(message: types.Message):
 
 
 # /name
-@router.message(Command("name"))
+@router.message(StateFilter(None), Command("name"))
 async def cmd_name(message: types.Message):
     await message.answer(
         f"Вас зовут <b>{message.from_user.full_name}</b>!",
@@ -55,7 +49,7 @@ async def cmd_name(message: types.Message):
 
 
 # /random
-@router.message(Command("random"))
+@router.message(StateFilter(None), Command("random"))
 async def cmd_random(message: types.Message):
     await message.answer(
         "Нажмите на кнопку, чтобы бот отправил число от 1 до 10.",
@@ -69,6 +63,14 @@ async def send_random_value(callback: types.CallbackQuery):
     await callback.answer()
 
 
+@router.message(StateFilter(None), Command("cancel"))
+async def cmd_cancel(message: types.Message):
+    await message.answer(
+        "Отменять нечего",
+         reply_markup=types.ReplyKeyboardRemove()
+    )
+
+
 @router.message(Command("cancel"))
 async def cmd_cancel(message: types.Message, state: FSMContext):
     # статус пользователя -> None
@@ -77,3 +79,8 @@ async def cmd_cancel(message: types.Message, state: FSMContext):
         "Вы прервали текущую операцию.",
          reply_markup=types.ReplyKeyboardRemove()
     )
+
+
+@router.message(StateFilter(None), ~F.text.in_(text.all_commands))
+async def not_a_command(message: types.Message):
+    await message.reply("Я вас не понимаю! Воспользуйтесь вкладкой 'Меню'")
